9ccdfea6f86115cdc287712148a11539281b8718
Hyunwns Kim <hyunwns78@yonsei.ac.kr>
2022-07-06 10:08:22 +0900

    [V6] 왼쪽 역삼각형 for문으로 구현
    왼쪽 역삼각형을 for문으로 구현

--- star3.py
+++ star3.py
@@ -1,6 +1,9 @@
 # star3_print.py
-print("*****")
-print(" ****")
-print("  ***")
-print("   **")
-print("    *")
+for i in range(5,0,-1): #1range(A, B, C) A부터 C 숫자만큼의 간격으로 B-1 까지의 정수 범위를 반환합니다.
+    for j in range(5-i):
+        print(' ',end="")
+    for j in range(i):
+        print('*',end="")
+        
+    print('')
+    
