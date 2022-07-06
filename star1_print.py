d425c3acd2ae6fcda1a4a0d6a1e93e3961c93dd5
김현준 <hyunwns78@yonsei.ac.kr>
2022-07-06 10:03:54 +0900

    : [V2] 왼쪽 삼각형 for 문으로 구현

--- star1_print.py
+++ star1_print.py
@@ -1,6 +1,13 @@
-# star1_print.py
-print("    *")
-print("   **")
-print("  ***")
-print(" ****")
-print("*****")
+# print("     *")
+# print("    **")
+# print("   ***")
+# print("  ****")
+# print(" *****")
+# print("******")
+
+for i in range(1,6): #1부터 6-1까지
+    for j in range(5-i):
+        print(' ',end="")
+    for j in range(i):
+        print('*',end="")
+    print('')
